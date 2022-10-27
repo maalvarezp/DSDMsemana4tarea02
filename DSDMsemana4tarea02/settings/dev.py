de3ab27c4a24378924234da70acfcad99aeba9f1
maximo <maximo@draco>
2022-10-26 22:31:39 -0500

    levanta cms en local y applicar cambios al front

---
+++ DSDMsemana4tarea02/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-*_97$$msu0%3^__m%wg+j#^6n3o=^^4s+8h9g&hy1q%h%@uhc!"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ home/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.fields import StreamField
+from wagtail import blocks
+from wagtail.admin.panels import FieldPanel
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.fields import RichTextField
+
+
+class HomePage(Page):
+    resume = RichTextField(blank=True)
+    body = StreamField([
+        ('heading', blocks.CharBlock(form_classname="title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ], use_json_field=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('resume'),
+        FieldPanel('body'),
+    ]
